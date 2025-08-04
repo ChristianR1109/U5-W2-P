f114113e0f9c5163b9af51ebedd76572b86eee60
Christian Ragonese <christianragonese1313@gmail.com>
2025-08-04 16:51:21 +0200

    base_configuration

---
+++ src/main/java/christian_ragonese/U5_W2_P/U5W2PApplication.java
@@ -0,0 +1,13 @@
+package christian_ragonese.U5_W2_P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2PApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2PApplication.class, args);
+	}
+
+}
---
+++ src/test/java/christian_ragonese/U5_W2_P/U5W2PApplicationTests.java
@@ -0,0 +1,13 @@
+package christian_ragonese.U5_W2_P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
